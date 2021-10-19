538b703d5d3e1b269c3d7f492ae4bdf218945d7a
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-10-19 15:20:56 +1300

    Add index files

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import './index.scss';
+
+class MyFlixApplication extends React.Component {
+  render() {
+    return (
+      <div className="my-flix">
+        <div>Good morning</div>
+      </div>
+    )
+  }
+}
