7acbfa87e0ad272bedea026f2999ff987e57a09a
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-10-25 13:05:36 +1300

    Add React-Bootstrap form style

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import propTypes from 'prop-types';
+// Rect Bootstrap
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export function RegistrationView(props) {
   const [ username, setUsername ] = useState('');
@@ -16,30 +19,31 @@ export function RegistrationView(props) {
   }
 
   return (
-    <form>
-      <h1>Registration page</h1>
-      <label>
-        *Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder={"Please set your username"} required />
-      </label>
-      <br/>
-      <label>
-        *Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder={"Please set your password"} required />
-      </label>
-      <br/>
-      <label>
-        *Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder={"example@mail.com"} required />
-      </label>
-      <br/>
-      <label>
-        Birthday:
-        <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
-      </label>
-      <br/>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Form>
+      <Form.Group className="mb-3" controlId='formUsername'>
+        <Form.Label>*Username:</Form.Label>
+        <Form.Control type='text' onChange={e => setUsername(e.target.value)}  placeholder='Please set your username' required />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId='formPassword'>
+        <Form.Label>*Password:</Form.Label>
+        <Form.Control type='password' onChange={e => setEmail(e.target.value)} placeholder='Please set your password' required />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId='formEmail'>
+        <Form.Label>*Email:</Form.Label>
+        <Form.Control type='email' onChange={e => setPassword(e.target.value)} placeholder='example@mail.com' required />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId='formBirthday'>
+        <Form.Label>Birthday:</Form.Label>
+        <Form.Control type='date' onChange={e => setBirthday(e.target.value)} />
+      </Form.Group>
+
+      <Button variant='primary' type='submit'  onClick={handleSubmit} >
+        Register
+      </Button>
+    </Form>
   )
 }
 
