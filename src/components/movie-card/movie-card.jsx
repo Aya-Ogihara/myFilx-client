eaf2a37892289c31c67d78d4df02d2fcbeed36e0
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-10-25 13:05:46 +1300

    Add React-Bootstrap card style

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import propTypes from 'prop-types';
+// Rect Bootstrap
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>
+    // return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>
+    return (
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={movie.ImagePath} crossOrigin='anonymous' />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button variant="primary" onClick={()=> onMovieClick(movie)} >Open</Button>
+        </Card.Body>
+      </Card>
+    )
   }
 }
 
